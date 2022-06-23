8fa8dd9fe2c7e7be30e45ebfed245d7ac6830b13
Samuel Arguello <sam.rene12@gmail.com>
2022-06-23 13:46:41 -0500

    style: created wrapper element card to contain shared styling

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import "./Card.css";
+
+function Card(props) {
+  //any additional class names that are added will be applied to the card class
+  const classes = "card " + props.className;
+  // props.children allows values to be input within the "<Card/>" tags
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
