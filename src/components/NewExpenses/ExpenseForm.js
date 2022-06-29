550c45119a0ddd68c5d03f735304b5cc3d3691a4
Samuel Arguello <sam.rene12@gmail.com>
2022-06-28 20:17:30 -0500

    feat: hid create expense button behind a add new expense button

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -101,6 +101,9 @@ const ExpenseForm = (props) => {
         ></input>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
